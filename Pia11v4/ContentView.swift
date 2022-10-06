5efc764826ba656780ba78f951db9680b5e8c3e5
th3w1ll <patrik.korab@gmail.com>
2022-10-06 10:40:13 +0200

    Ändrat samma text

--- Pia11v4/ContentView.swift
+++ Pia11v4/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Tjena")
-            Text("Hejsan Svejsan")
+            Text("Hejsan")
         }
         .padding()
     }
