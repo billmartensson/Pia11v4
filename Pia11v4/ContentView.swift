f8293c81870113f61f5d62d5faf7dda335f5ab8e
Bill Mårtensson <bill@magictechnology.se>
2022-10-06 13:57:49 +0200

    Uträkning

--- Pia11v4/ContentView.swift
+++ Pia11v4/ContentView.swift
@@ -8,10 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var thenumber = 0
+    
+    @State var usernumber = ""
+    
+    @State var errortext = ""
+    
     var body: some View {
         VStack {
-            Text("Tjena tjena tjena")
-            Text("Hejsan")
+            Text(String(thenumber))
+                .font(.largeTitle)
+            
+            Text(errortext)
+                .fontWeight(.bold)
+                .foregroundColor(Color.red)
+            
+            TextField("Skriv siffra", text: $usernumber).keyboardType(.numbersAndPunctuation)
+            
+            Button(action: {
+                calcstuff(howcalc: "plus")
+            }, label: {
+                Text("PLUS")
+            }).padding()
+            
+            Button(action: {
+                calcstuff(howcalc: "minus")
+            }, label: {
+                Text("MINUS")
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                calcstuff(howcalc: "multiplicera")
+            }, label: {
+                Text("MULTIPLICERA")
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                calcstuff(howcalc: "dividera")
+            }, label: {
+                Text("DIVIDERA")
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                thenumber = 0
+            }, label: {
+                Text("RESET")
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    func calcstuff(howcalc : String) {
+        
+        errortext = ""
+        
+        let calcsiffran = Int(usernumber)
+        
+        if(calcsiffran != nil) {
+            if(howcalc == "plus") {
+                thenumber = thenumber + calcsiffran!
+            }
+            
+            if(howcalc == "minus") {
+                thenumber = thenumber - calcsiffran!
+            }
+            
+            if(howcalc == "multiplicera") {
+                thenumber = thenumber * calcsiffran!
+            }
+            
+            if(howcalc == "dividera") {
+                if(calcsiffran != 0) {
+                    thenumber = thenumber / calcsiffran!
+                } else {
+                    errortext = "Division med noll!!"
+                }
+            }
+            
+            usernumber = ""
         }
     }
 }
