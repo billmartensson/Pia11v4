3133be92fa6c2104fad941151b10bbb4fb452eab
Bill Mårtensson <bill@magictechnology.se>
2022-10-06 09:21:08 +0200

    Initial Commit

---
+++ Pia11v4/Pia11v4App.swift
@@ -0,0 +1,17 @@
+//
+//  Pia11v4App.swift
+//  Pia11v4
+//
+//  Created by Bill Martensson on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct Pia11v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
